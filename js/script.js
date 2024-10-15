febaed3c92a3d65d493be78a885258e1c006e3d7
haha123zhang4 <ztt3146265177@163.com>
2024-10-15 21:38:53 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -78,7 +78,7 @@ const mainStage = new Stage("main-canvas");
 const stages = [trailsStage, mainStage];
 
 //随机文字烟花内容
-const randomWords = ["祝张佳烁生日快乐", "心想事成"];
+const randomWords = ["祝张佳烁生日快乐", "心想事成","万事如意","天天开心","好运连连"];
 const wordDotsMap = {};
 randomWords.forEach((word) => {
 	wordDotsMap[word] = MyMath.literalLattice(word, 3, "Gabriola,华文琥珀", "90px");
